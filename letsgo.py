2c40f24c2349d34e22f399d28992cd74b9782a23
Keem Codes <akeeminbox@gmail.com>
2021-10-03 22:32:05 -0400

    first commit

---
+++ letsgo.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+home_dir = os.system("cd ~")
+print("`cd ~` ran with exit code %d" % home_dir)
+
+file = open('remix2.txt', 'r')
+lines = file.readlines()
+
+for index, line in enumerate(lines):
+    print("Download Log: ",index, line.strip())
+    home_dir = os.system("waybackpack {} -d ~/Downloads/lala/{}".format(line.strip(), index))
+    print(home_dir)
+    time.sleep(3)
+
+
+    
+file.close()
